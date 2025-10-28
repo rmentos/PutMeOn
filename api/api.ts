6902dc610d6312a4537a7145072ffbf1c94278b6
Mentos <78933807+rmentos@users.noreply.github.com>
2025-10-28 14:36:01 -0700

    url for school

--- api/api.ts
+++ api/api.ts
@@ -1,5 +1,5 @@
 // api/api.ts
-const API_URL = "http://192.168.1.149:3001";
+const API_URL = "http://localhost:3001";
 import { SpotifyTrack } from "@/app/screens/suggestionscreen";
 import { db } from "@/src/firebase";
 import { addDoc, collection, getDocs, query, where } from "firebase/firestore";
